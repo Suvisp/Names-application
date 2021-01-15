3a638997f2b4b6c441166c453b3e6176e03532e8
suvisp <suvi.ahramo@gmail.com>
2021-01-15 22:43:25 +0200

    frontend draft to git

---
+++ react_frontend/src/App.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'
+import './App.css';
+import TotalNames from './components/TotalNames'
+import SelectFilter from './components/SelectFilter'
+import FilterNames from './components/FilterNames'
+import nameService from './services/restclient'
+
+function App() {
+  const [names, setNames] = useState([])
+
+
+  useEffect(() => {
+    nameService
+      .getAll()
+      .then(initialNames => {
+        setNames(initialNames)
+      })
+  }, [])
+
+  return (
+    <div class="flex-container">
+      <SelectFilter style="order: 1" names={names} />
+      <TotalNames style="order: 2" names={names} />
+      <FilterNames style="order: 3" names={names} />
+    </div>
+  );
+}
+
+export default App;
---
+++ react_frontend/src/components/FilterNames.jsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+
+const FilterNames = ({ names }) => {
+    const [searchterm, setSearchterm] = useState([])
+    const [amountOfName, setAmountOfName] = useState([])
+
+    useEffect(() => {
+        const amountOfName =! searchterm
+        ? names
+        : names.filter((m) => m.name === searchterm).map((mm) => mm.amount)
+        console.log('searchterm', searchterm)
+        console.log('amountOfName', amountOfName)
+        setAmountOfName(amountOfName);
+    }, [searchterm]);
+
+    return (
+        <div class="side">
+            <input type='text' id='give a name' name='insert a name' value={searchterm} onChange={(e) => setSearchterm(e.target.value)} />
+            <p>{amountOfName}</p>
+        </div>
+    )
+}
+
+export default FilterNames;
